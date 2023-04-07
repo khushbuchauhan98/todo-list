3652aac4bf18c33fd45507f3e7c35cc5ce138fb6
khushbu19998@outlook.com <khushbu19998@outlook.com>
2023-04-07 22:46:19 +0530

    todo-final

--- app.js
+++ app.js
@@ -4,31 +4,39 @@ render = () => {
     right.innerHTML = ""
     users.map((user) => {
         let divs = document.createElement('div')
+        divs.classList.add('students');
+        let checl = document.createElement('input')
+        checl.type = 'checkbox'
+        // checl.classList.add('checks')
+        checl.id = 'checks'
         let divs1 = document.createElement('div')
+        divs1.id = 'bg'
+        // divs1.classList.add('bg')
         let p1 = document.createElement('p1')
         let p2 = document.createElement('p2')
         let p3 = document.createElement('p3')
-        let checl = document.createElement('input')
-        divs.classList.add('students');
+       
+       
         p1.classList.add('block')
         p2.classList.add('block');
         p1.classList.add('p1')
         p3.classList.add('d')
-
-        divs1.id = 'bg'
+        p1.id='p1'
+        p2.id='p2'
+        p3.id='p3'
+       
         p1.innerText = user.task
         p2.innerText = user.desc;
         p3.innerText = user.date;
-        checl.type = 'checkbox'
-        checl.id = 'checks'
+        
         //     checl.addEventListener('click',(e)=>{
         //         let bg=document.getElementById('bg')
         // bg.classList.add('bg')
         //     })
         // checl.onclick(fail())
-
-        divs.appendChild(checl)
         right.appendChild(divs);
+        divs.appendChild(checl)
+       
         divs.append(divs1)
 
         divs1.appendChild(p1);
@@ -36,8 +44,9 @@ render = () => {
         divs1.appendChild(p3)
 
         checl.onclick = function () {
+            
             if (checl.checked == true) {
-
+                checl.checked = true;
                 divs1.classList.add('bg')
                 p1.classList.add('line')
                 p2.classList.add('line')
@@ -45,9 +54,11 @@ render = () => {
                 // checl.checked =true;
             }
             else {
-
-                // divs1.classList.remove('bg')
-                checl.checked = true;
+                divs1.classList.remove('bg')
+                p1.classList.remove('line')
+                p2.classList.remove('line')
+                p3.classList.remove('line')
+                // checl.checked = true;
             }
 
         }
@@ -60,12 +71,13 @@ submit = () => {
     let task = document.getElementById('task');
     let desc = document.getElementById('desc');
     let date = document.getElementById('date');
+    
     let tempuser = {
         task: task.value,
         desc: desc.value,
-        date: date.value
+        date: date.value,
+        
     }
-
     if (task.value == '') {
         task.placeholder = "task name"
         task.classList.add('placeholderscolor')
@@ -92,20 +104,22 @@ submit = () => {
             date.classList.remove('placeholderscolor')
         }, 2000);
     }
-    if (task.value != '' && desc.value != '' && date.value != '') {
+    if (task.value != '' && desc.value != '' && date.value != '' ) {
         users.push(tempuser);
         task.value = '';
         desc.value = ''
         date.value = '';
-        render()
+       
+        render();
     }
-    else {
+   
+    // else {
         // users.push(tempuser);
         // task.value = '';
         //     desc.value = ''
         //     date.value = '';
         // render()
-    }
+    // }
     //   let tasks=users.filter((user)=>{
     //     return user.task==task.value
     // })
@@ -120,7 +134,7 @@ submit = () => {
     // else{
     //     alert('same task exists')
     // }
-
+    
 
     console.log(users)
 }

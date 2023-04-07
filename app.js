6eef85eb2f406c051957947407e3372f59791d99
khushbu19998@outlook.com <khushbu19998@outlook.com>
2023-04-07 11:11:28 +0530

    vfs

--- app.js
+++ app.js
@@ -14,19 +14,19 @@ render = () => {
         p2.classList.add('block');
         p1.classList.add('p1')
         p3.classList.add('d')
-       
+
         divs1.id = 'bg'
         p1.innerText = user.task
         p2.innerText = user.desc;
         p3.innerText = user.date;
         checl.type = 'checkbox'
-        checl.id='checks'
+        checl.id = 'checks'
         //     checl.addEventListener('click',(e)=>{
         //         let bg=document.getElementById('bg')
         // bg.classList.add('bg')
         //     })
         // checl.onclick(fail())
-        
+
         divs.appendChild(checl)
         right.appendChild(divs);
         divs.append(divs1)
@@ -34,19 +34,22 @@ render = () => {
         divs1.appendChild(p1);
         divs1.appendChild(p2);
         divs1.appendChild(p3)
-        
+
         checl.onclick = function () {
-            if(checl.checked ==true){
-               
+            if (checl.checked == true) {
+
                 divs1.classList.add('bg')
+                p1.classList.add('line')
+                p2.classList.add('line')
+                p3.classList.add('line')
                 // checl.checked =true;
             }
-            else{
-                
+            else {
+
                 // divs1.classList.remove('bg')
-                checl.checked =true;
+                checl.checked = true;
             }
-           
+
         }
 
     })
@@ -62,38 +65,47 @@ submit = () => {
         desc: desc.value,
         date: date.value
     }
-   
-    if(task.value==''){
-       task.placeholder="task name"
-       task.classList.add('placeholderscolor')
-       setTimeout(() => {
-        task.placeholder="Task name here"
-        task.classList.remove('placeholderscolor')
-       }, 2000);
+
+    if (task.value == '') {
+        task.placeholder = "task name"
+        task.classList.add('placeholderscolor')
+        setTimeout(() => {
+            task.placeholder = "Task name here"
+            task.classList.remove('placeholderscolor')
+        }, 2000);
+
     }
-     if(desc.value==''){
-        desc.placeholder="Add Description"
+    if (desc.value == '') {
+        desc.placeholder = "Add Description"
         desc.classList.add('placeholderscolor')
         setTimeout(() => {
-            desc.placeholder="Description"
+            desc.placeholder = "Description"
             desc.classList.remove('placeholderscolor')
         }, 2000);
-     }
-     if(date.value==''){
-        date.placeholder="Add Date!!"
+    }
+
+    if (date.value == '') {
+        date.placeholder = "Add Date!!"
         date.classList.add('placeholderscolor')
         setTimeout(() => {
-            date.placeholder="Due Date"
+            date.placeholder = "Due Date"
             date.classList.remove('placeholderscolor')
         }, 2000);
-     }
-     else{
+    }
+    if (task.value != '' && desc.value != '' && date.value != '') {
         users.push(tempuser);
         task.value = '';
-            desc.value = ''
-            date.value = '';
+        desc.value = ''
+        date.value = '';
         render()
-     }
+    }
+    else {
+        // users.push(tempuser);
+        // task.value = '';
+        //     desc.value = ''
+        //     date.value = '';
+        // render()
+    }
     //   let tasks=users.filter((user)=>{
     //     return user.task==task.value
     // })
@@ -104,11 +116,11 @@ submit = () => {
     //     date.value = '';
     //     render()
     // }
-   
+
     // else{
     //     alert('same task exists')
     // }
-    
-   
+
+
     console.log(users)
 }
